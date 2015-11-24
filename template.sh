c90ca02d025915333ea87a41db3686822812ab8c
Chun Cheng Lin <jinsenglin@users.noreply.github.com>
2015-11-24 23:59:39 +0800

    Create template.sh

---
+++ template.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -e # This tells bash that it should exit the script if any statement returns a non-true return value.
+set -x # Traces of each command plus its arguments are printed to standard output after the commands have been expanded but before they are executed.
+
+function clean_up {
+  rm -rf tmp.*
+
+  echo cleaned
+  exit
+}
+trap clean_up SIGINT SIGTERM
+
+        # SIGINT: 2, interrupt process, [ctrl] + [c]
+        # SIGTERM: 15, terminate process
+
+function echo_this_dir_abs_path {
+  pushd `dirname $0` > /dev/null
+  local SCRIPTPATH=`pwd -P` # Display the physical current working directory
+  popd > /dev/null
+
+  echo $SCRIPTPATH
+}
+
+function mk_tmp_file {
+  mktemp tmp.XXXX
+}
+mk_tmp_file
+
+function mk_tmp_dir {
+  mktemp -d tmp.XXXX
+}
+mk_tmp_dir
+
+function infinite_loop {
+  while true; do
+    $1
+  done
+}
+
+function dummy {
+  date
+  sleep 1
+}
+
+infinite_loop dummy
