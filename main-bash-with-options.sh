5fa806318c33f3def4709dd084a8ca179fc86025
Jim Lin <jimlintw922@gmail.com>
2017-02-04 15:40:05 +0800

    update main-bash-with-options.sh

--- main-bash-with-options.sh
+++ main-bash-with-options.sh
@@ -1,18 +1,52 @@
 #!/bin/bash
 
+# r config
+# r help
+function dump_options() {
+    local _format="%-10s = %s\n"
+
+    printf "$_format" config $config
+    printf "$_format" help $help
+}
+
 function next_step() {
-    echo next_step
+    dump_options
 }
 
 function show_help() {
-    echo Usage
+    local _format1="%4s, %-20s%s\n"
+    local _format2="  %-24s%s\n"
+    local _format3="  %s\n"
+
+    echo Usage: $0 [OPTIONS]
+    echo
+    echo One line description
+    echo
+    echo Options:
+    echo
+    printf "$_format1" "-c" "--config=./" "Location of client config files"
+    printf "$_format1" "-h" "--help" "Print usage"
+    echo
+    echo Environment Variables:
+    echo
+    printf "$_format2" "x_config" "Location of client config files"
+    echo
+    echo Examples:
+    echo
+    printf "$_format3" "$0"
+    printf "$_format3" "$0 -h"
+    printf "$_format3" "$0 --help"
+    printf "$_format3" "$0 -c /tmp"
+    printf "$_format3" "$0 --config /tmp"
 }
 
-# set config
-# set help
+# w config
+# w help
 function parse_options() {
-    set -e
+    # set  default
+    help=
 
+    set -e
     while [[ $# -gt 0 ]]
     do
         key=$1
@@ -34,21 +68,24 @@ function parse_options() {
 
         shift # past argument
     done
-
     set +e
 }
 
-# set help
+# w config
 function parse_envvars() {
     # set default
-    help=${x_help:-}
+    config=${x_config:-./}
 }
 
-# main
-parse_envvars
-parse_options $@
-if [ $help ]; then
+# r help
+function main() {
+    parse_envvars
+    parse_options $@
+    if [ $help ]; then
         show_help
-else
+    else
         next_step
-fi
+    fi
+}
+
+main $@
