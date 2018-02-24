6989d5f1374bd89a891dfbd8918bc43d9b341c73
Jim Lin <jimlintw922@gmail.com>
2018-02-24 15:48:11 +0800

    update redirect-io.sh

--- redirect-io.sh
+++ redirect-io.sh
@@ -8,8 +8,8 @@ ls 1
 
 # redirect output from console to file
 exec 3> this.log # use fd3 to open file this.log (only write mode)
-exec 9>&1 # backup fd1
-exec 8>&2 # backup fd2
+exec 9>&1 # use fd9 to backup fd1
+exec 8>&2 # use fd8 to backup fd2
 exec 1>&3 # redirect std out to fd3
 exec 2>&3 # redirect std err to fd3
 
